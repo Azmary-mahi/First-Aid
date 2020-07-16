abf4aa27408dc6d7cdb4af04cdaeff0421c0e4e8
Azmary-mahi <azmary.mahi@gmail.com>
2020-07-16 21:56:18 +0600

    commit5

---
+++ app/src/main/java/com/example/firstaidkit/heartattack.java
@@ -0,0 +1,14 @@
+package com.example.firstaidkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class heartattack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heartattack);
+    }
+}
